a7f27968fdecf91c1ca8c5bddd1f0891fa46b9ea
Cynthia Colin <cynthiacolin@hotmail.fr>
2021-09-29 14:30:55 +0200

    css page accueil

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -4,29 +4,30 @@ let url = `http://localhost:3000/api/cameras`;
 
 function getCamera (){
 fetch(url)
-  .then (data => data.json())
-  .then(data => console.log(data))
-  .then(data => {
-    let camera = data.map; 
-     for (let i=0; i < camera.length; i++){
-      let name = camera[i].name;
-      let imageUrl = camera[i].imageUrl;
-      let description = camera[i].description;
-      let price = camera[i].price /100;
-      let id  = camera[i]._id;
+  .then ((data) => data.json())
+   .then((data) => {
+    
+     for (let i=0; i < data.length; i++){
+      let name = data[i].name;
+      let imageUrl = data[i].imageUrl;
+      let description = data[i].description;
+      let price = data[i].price /100;
+      let id  = data[i]._id;
 
-      let card =`<div class="main__card">
+      let card =`<div class="main__card border border-secondary rounded" >
       <img src="${imageUrl}" class="card-img-top" alt="camera">
      <div class="main__card-body">
-        <h3 class="main__card-title">${name}</h3>
-        <p class="main__card-text">${description}</p>
-        <p class="main__card-price"><medium class="text-muted">${price} €</medium></p>
+        <h3 class="main__card-title text-center">${name}</h3>
+        <p class="main__card-text text-justify">${description}</p>
+        <p class="main__card-price text-right"><medium class="text-muted">${price} €</medium></p>
       </div>
-    </div>`;
+    </div>`
     
-    document.querySelector('.main').innerHtml = card;
-    }
- })
+    document.querySelector('.main').innerHTML += card;
+     }
+    
+  })
+ 
 //  .catch(function(error) {
 //   console.log(error);}
 }
